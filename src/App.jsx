bbb4384b398e2e075d7eacbc69d8048818cf4009
channykim00 <hadeskim0429@gmail.com>
2025-06-30 20:25:17 +0900

    feat: 라우터 구조 개선 및 ReceiveFile 페이지 라우트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,23 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import MainLayout from "@/components/layout/MainLayout";
 
 import Home from "@/pages/Home";
+import ReceiveFile from "@/pages/ReceiveFile";
 
 function App() {
   return (
     <Router>
-      <MainLayout>
-        <Routes>
+      <Routes>
+        <Route element={<MainLayout />}>
           <Route
             path="/"
             element={<Home />}
           />
-        </Routes>
-      </MainLayout>
+          <Route
+            path="/receiveFile"
+            element={<ReceiveFile />}
+          />
+        </Route>
+      </Routes>
     </Router>
   );
 }
--- src/components/Layout/MainLayout.jsx
+++ src/components/Layout/MainLayout.jsx
@@ -1,13 +1,16 @@
+import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 
-const MainLayout = ({ children }) => {
+const MainLayout = () => {
   return (
     <div className="flex h-screen">
       <Sidebar />
       <div className="flex flex-1 flex-col overflow-y-auto">
         <Header />
-        <main className="flex-1 overflow-y-auto bg-gray-50 p-5">{children}</main>
+        <main className="flex-1 overflow-y-auto bg-gray-50 p-5">
+          <Outlet />
+        </main>
       </div>
     </div>
   );
