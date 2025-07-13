7a7712e545816a18f44d101f96b1812b27c00f57
channykim00 <hadeskim0429@gmail.com>
2025-07-13 22:55:58 +0900

    fix: 알림이 여러번 뜨는 문제, useEffect의 클린업 함수로 해결

--- electron/preload/preload.js
+++ electron/preload/preload.js
@@ -9,11 +9,18 @@ contextBridge.exposeInMainWorld("api", {
   getLinkByUniqueUrl: (uniqueUrl) => ipcRenderer.invoke("get-link-by-url", uniqueUrl),
   openFolder: (path) => ipcRenderer.invoke("open-folder", path),
 
+  getLinkList: () => ipcRenderer.invoke("get-link-list"),
+  deleteLinkByUrl: (uniqueUrl) => ipcRenderer.invoke("delete-link-by-url", uniqueUrl),
+
   getDownloadHistory: () => ipcRenderer.invoke("get-download-history"),
   deleteDownloadHistory: (fileId) => ipcRenderer.invoke("delete-download-history", fileId),
 
   onAutoAcceptUpload: (callback) => ipcRenderer.on("auto-accept-upload", callback),
   onShowUploadAccept: (callback) => ipcRenderer.on("show-upload-accept", callback),
+
+  offAutoAcceptUpload: (callback) => ipcRenderer.removeListener("auto-accept-upload", callback),
+  offShowUploadAccept: (callback) => ipcRenderer.removeListener("show-upload-accept", callback),
+
   sendAcceptedUpload: (uploadData) => ipcRenderer.send("accept-upload", { uploadData }),
 
   getUploadRequests: () => ipcRenderer.invoke("get-upload-requests"),
